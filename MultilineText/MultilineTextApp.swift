fcd1bd2ee5eb5c012914d86b45344074d7dba7bb
InnaChystiakova <inna_islyamova@ukr.net>
2024-05-03 10:46:24 +0100

    Initial Commit

---
+++ MultilineText/MultilineTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultilineTextApp.swift
+//  MultilineText
+//
+//  Created by Inna Chystiakova on 03/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct MultilineTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
